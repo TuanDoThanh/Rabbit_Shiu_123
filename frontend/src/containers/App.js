48a75203e664b9199d84cce8299da80a9f9531ba
minhphuc77 <phuctmbdaf200019@fpt.edu.vn>
2023-03-16 15:00:46 +0700

    + Login - register + address API controller, user API controller

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomeContainer from './home.container'
+import LoginRegisterContainer from './login.register.container'
+import VerifyRegisterAccountContainer from './verify.register.account.container'
+import ForgotPasswordContainer from './forgot.password.container'
 const App = () => (
   <Router>
     <Switch>
       <Route exact path='/' component={HomeContainer} />
+      <Route exact path='/login_register' component={LoginRegisterContainer} />
+      <Route exact path='/confirm/:token' component={VerifyRegisterAccountContainer} />
+      <Route exact path='/forgotpass/' component={ForgotPasswordContainer} />
     </Switch>
   </Router>
 )
