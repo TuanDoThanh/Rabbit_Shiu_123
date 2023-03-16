0f6195af2b03738d1f468653eb61de1c2d097df6
Thanhtinh14102002 <tinhltbdaf200031@fpt.edu.vn>
2023-03-16 16:13:23 +0700

    + Update css + Update App.js

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -4,11 +4,11 @@ import HomeContainer from './home.container'
 import LoginRegisterContainer from './login.register.container'
 import VerifyRegisterAccountContainer from './verify.register.account.container'
 import ForgotPasswordContainer from './forgot.password.container'
-import ProfileContainer from './profile.container'
 import ProductDetailContainer from './product.detail.container'
-import HistoryPurchase from './history.purchase.container'
+import ProfileContainer from './profile.container'
 import CartContainer from './cart.container'
 import VerifyPaymentContainer from './verify.payment.container'
+import HistoryPurchase from './history.purchase.container'
 const App = () => (
   <Router>
     <Switch>
@@ -16,11 +16,11 @@ const App = () => (
       <Route exact path='/login_register' component={LoginRegisterContainer} />
       <Route exact path='/confirm/:token' component={VerifyRegisterAccountContainer} />
       <Route exact path='/forgotpass/' component={ForgotPasswordContainer} />
-      <Route exact path='/profile/:email' component={ProfileContainer} />
-      <Route exact path='/purchase_history' component={HistoryPurchase} />
       <Route exact path='/product/:id' component={ProductDetailContainer} />
+      <Route exact path='/profile/:email' component={ProfileContainer} />
       <Route exact path='/cart' component={CartContainer} />
       <Route exact path='/payment/:token' component={VerifyPaymentContainer} />
+      <Route exact path='/purchase_history' component={HistoryPurchase} />
     </Switch>
   </Router>
 )
